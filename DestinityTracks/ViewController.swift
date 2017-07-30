6f5b695842ffa3552f97fbba46de42061c19bff7
Juan Morillo <juanmorillios@gmail.com>
2017-07-30 19:16:08 +0200

    Update Code

--- DestinityTracks/ViewController.swift
+++ DestinityTracks/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+  
+  var audioPlayer : AVAudioPlayer?
+  let tracksArray = ["Track1","Track2","Track3","Track4",
+                     "Track5","Track6","Track7","Track8"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  @IBAction func buttonPlayTrackPressed(_ sender: UIButton) {
+    playTrack(trackFileName: tracksArray[sender.tag - 1])
   }
-
-
+  
+  @IBAction func buttonStopPlayingPressed(_ sender: UIButton) {
+    audioPlayer?.stop()
+  }
+  
+  @IBAction func sliderVolumeTracks(_ sender: UISlider) {
+    if (audioPlayer != nil) {
+      audioPlayer?.volume = sender.value
+    }
+  }
+  
+  func playTrack(trackFileName :String){
+    let trackURL = Bundle.main.url(forResource: trackFileName, withExtension: "mp3")!
+    
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: trackURL)
+      guard let audioPlayer = audioPlayer else { return }
+      audioPlayer.prepareToPlay()
+      audioPlayer.numberOfLoops = 1
+      audioPlayer.play()
+    } catch let error as NSError {
+      print(error.description)
+    }
+  }
 }
 
